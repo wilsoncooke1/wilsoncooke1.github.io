9e9caebb520373ce4a175c10625f4d380b0f7c88
wilsoncooke1 <wacallemail@gmail.com>
2025-07-20 17:37:59 -0400

    Update vines.js

--- vines.js
+++ vines.js
@@ -27,7 +27,7 @@ for (let i = 0; i < 25; i++) {
 }
 
 function draw() {
-  ctx.fillStyle = 'rgba(0, 0, 0, 0.05)';
+  ctx.fillStyle = 'rgba(0, 255, 0, 0.05)';
   ctx.fillRect(0, 0, width, height);
 
   ctx.strokeStyle = '#0077ff';
